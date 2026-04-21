686f6eb44dc7a17a44cee5e2da2b17c8d13f1a9f
GrandTojo <tojomayose@gmail.com>
2026-04-21 08:17:13 +0300

    feat: ajout schemas, models, router citoyens et get_db

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,17 @@ from fastapi import FastAPI
 import nacl.signing
 import nacl.encoding
 import json
-from database  import engine, Base
+from database import engine, Base
 import models
+from routers import citoyens
 
 app = FastAPI(title="Mada-ID Backend")
+
+# Crée toutes les tables dans PostgreSQL
 Base.metadata.create_all(bind=engine)
+
+# 🔌 Branche le guichet citoyens
+app.include_router(citoyens.router)
 
 # --- MISSION 1 : L'ACCUEIL ---
 @app.get("/")
@@ -18,10 +24,8 @@ def dire_bonjour():
 def creer_des_cles():
     cle_secrete = nacl.signing.SigningKey.generate()
     cle_publique = cle_secrete.verify_key
-    
     cle_secrete_texte = cle_secrete.encode(encoder=nacl.encoding.HexEncoder).decode('utf-8')
     cle_publique_texte = cle_publique.encode(encoder=nacl.encoding.HexEncoder).decode('utf-8')
-    
     return {
         "message": "Voici les clés pour l'Institution !",
         "cle_publique": cle_publique_texte,
@@ -37,17 +41,12 @@ def signer_un_diplome():
         "grade": "Licence Informatique",
         "annee": 2024
     }
-    
     cle_secrete = nacl.signing.SigningKey.generate()
-    
     diplome_texte = json.dumps(diplome)
     diplome_octets = diplome_texte.encode('utf-8')
-    
     document_signe = cle_secrete.sign(diplome_octets)
-    
     signature_texte = nacl.encoding.HexEncoder.encode(document_signe.signature).decode('utf-8')
     cle_publique = cle_secrete.verify_key.encode(encoder=nacl.encoding.HexEncoder).decode('utf-8')
-    
     return {
         "message": "Diplôme certifié avec succès !",
         "donnees_diplome": diplome,
@@ -55,10 +54,9 @@ def signer_un_diplome():
         "cle_publique_pour_verifier": cle_publique
     }
 
-
+# --- TEST CONNEXION BASE DE DONNÉES ---
 try:
     engine.connect()
     print("✅ SUCCÈS : FastAPI est bien connecté à PostgreSQL !")
 except Exception as e:
     print("❌ ERREUR de connexion :", e)
-
